f16f7b2fe7291beb9eaa81aefb3f0e4955b40710
zorigoosw06d039 <zorigoo_mandakh@yahoo.com>
2014-05-30 15:35:14 +0800

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Text = "Form1";
+            this.Text = "Zorigoo edit";
         }
 
         #endregion
